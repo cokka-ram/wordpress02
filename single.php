0b2503b695760699134d0687912877979b650c9c
高山博子 <148430yuno@gmail.com>
2021-09-16 19:32:00 +0900

    ９月９日までの作業

---
+++ single.php
@@ -0,0 +1,25 @@
+<?php 
+get_header();
+?>
+<?php if ( shortcode_exists( 'wp-structuring-markup-breadcrumb' ) ) : ?>
+<nav class="breadcrumb">
+	<?php  echo do_shortcode( '[wp-structuring-markup-breadcrumb]' ); ?>
+</nav>
+<?php endif; ?>
+<main>
+	<?php 
+	if( have_posts() ) : while ( have_posts() ) : the_post();?>
+		<article class="news-article">
+			<h1><?php the_title(); ?></h1>
+			<div class="post-date">
+        <time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time( get_option( 'date_format' ) ); ?></time>
+      </div>			
+			<?php the_content(); ?>
+		</article>			
+	<?php endwhile; endif; ?>
+</main>
+
+<?php
+get_footer();
+
+ 
